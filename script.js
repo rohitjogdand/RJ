e483754fe1280baea8a43a20a880546871b3fb8b
rohit jogdand <rohitjogdand17@gmail.com>
2026-02-08 11:36:41 +0530

    changes for no button moving

---
+++ script.js
@@ -0,0 +1,14 @@
+const no = document.querySelector("#no")
+
+const moveBtn = () =>{
+    const maxWidth = window.innerWidth - no.clientWidth;
+    const maxHeight = window.innerHeight - no.clientHeight;
+
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
+    no.style.position = 'fixed';
+    no.style.left = randomX + 'px';
+    no.style.top = randomY + 'px';
+}
+
+no.addEventListener('mouseover',moveBtn)
